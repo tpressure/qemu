62759f0e277e5bf5e7c3647e1bbc1ed1bebd13db
Zhao Liu <zhao1.liu@intel.com>
2023-11-30 22:41:48 +0800

    hw/cpu/drawer: Abstract cpu-drawer level as topology device
    Abstract drawer level as a topology device "cpu-drawer" to allow user to
    create drawer level topology from cli and later the cpu-drawers could be
    added into topology tree.
    
    In addition, mark the cpu-drawer as DEVICE_CATEGORY_CPU_DEF category to
    indicate it belongs to the basic CPU definition and should be created
    from cli before board initialization.
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>

---
+++ include/hw/cpu/drawer.h
@@ -0,0 +1,38 @@
+/*
+ * CPU drawer abstract device
+ *
+ * Copyright (c) 2023 Intel Corporation
+ * Author: Zhao Liu <zhao1.liu@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HW_CPU_DRAWER_H
+#define HW_CPU_DRAWER_H
+
+#include "hw/core/cpu-topo.h"
+#include "hw/qdev-core.h"
+
+#define TYPE_CPU_DRAWER "cpu-drawer"
+
+OBJECT_DECLARE_SIMPLE_TYPE(CPUDrawer, CPU_DRAWER)
+
+struct CPUDrawer {
+    /*< private >*/
+    CPUTopoState obj;
+
+    /*< public >*/
+};
+
+#endif /* HW_CPU_DRAWER_H */
